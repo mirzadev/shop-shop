b07d3396b32c5c0388eb0e2984db078e0f43a69e
Mirza Abdul Awal <awal.mirza2016@gmail.com>
2021-11-07 23:13:37 -0500

    test for cart

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,7 +2,13 @@ import { useReducer } from 'react';
 import {
       UPDATE_PRODUCTS,
       UPDATE_CATEGORIES,
-      UPDATE_CURRENT_CATEGORY
+      UPDATE_CURRENT_CATEGORY,
+      ADD_TO_CART,
+      ADD_MULTIPLE_TO_CART,
+      REMOVE_FROM_CART,
+      UPDATE_CART_QUANTITY,
+      CLEAR_CART,
+      TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -28,6 +34,62 @@ export const reducer = (state, action) => {
                   return {
                         ...state,
                         currentCategory: action.currentCategory
+                  };
+
+            // for Add to Cart
+            case ADD_TO_CART:
+                  return {
+                        ...state,
+                        cartOpen: true,
+                        cart: [...state.cart, action.product]
+                  };
+
+
+            // to add multiple items to cart
+            case ADD_MULTIPLE_TO_CART:
+                  return {
+                        ...state,
+                        cart: [...state.cart, ...action.products],
+                  };
+
+            // to remove items from cart
+            case REMOVE_FROM_CART:
+                  let newState = state.cart.filter(product => {
+                        return product._id !== action._id;
+                  });
+
+                  return {
+                        ...state,
+                        cartOpen: newState.length > 0,
+                        cart: newState
+                  };
+
+            // to update the item quantity in the cart
+            case UPDATE_CART_QUANTITY:
+                  return {
+                        ...state,
+                        cartOpen: true,
+                        cart: state.cart.map(product => {
+                              if (action._id === product._id) {
+                                    product.purchaseQuantity = action.purchaseQuantity;
+                              }
+                              return product;
+                        })
+                  };
+
+            // to clear items from the cart
+            case CLEAR_CART:
+                  return {
+                        ...state,
+                        cartOpen: false,
+                        cart: []
+                  };
+
+            // to toggle items in the cart
+            case TOGGLE_CART:
+                  return {
+                        ...state,
+                        cartOpen: !state.cartOpen
                   };
       }
 };
